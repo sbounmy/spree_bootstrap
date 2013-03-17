f1d8b63d7063ae0728bf1ffd5a9d0a32ae23199d
Stephane Bounmy <stephanebounmy@gmail.com>
2013-03-17 16:22:47 +0100

    added cart template

---
+++ app/overrides/spree/disabled.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/orders/edit",
+                     :name => "promo_cart_coupon_code_field",
+                     :replace => "",
+                     :disabled => true,
+                     :original => "c11d9a1996fb86e992aba19035074cf5f688dea2")
