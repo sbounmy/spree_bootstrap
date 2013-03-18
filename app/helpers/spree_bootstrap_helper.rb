94a7e1182c9aaa7773b478ab94594da1e127db53
Stephane Bounmy <stephanebounmy@gmail.com>
2013-03-18 23:12:50 +0100

    wishlist / user_passwords and missing en i18n

--- app/helpers/spree_bootstrap_helper.rb
+++ app/helpers/spree_bootstrap_helper.rb
@@ -4,4 +4,23 @@ module SpreeBootstrapHelper
     options[:class] += " icon-#{icon_name}"
     content_tag(:i, '', options)
   end
+
+  def flash_class(level)
+    case level
+    when :notice then "alert-info"
+    when :error then "alert-error"
+    when :alert then "alert-warning"
+    end
+  end
+
+  def flash_messages(opts = {})
+    opts[:ignore_types] = [:commerce_tracking].concat(Array(opts[:ignore_types]) || [])
+
+    flash.each do |msg_type, text|
+      unless opts[:ignore_types].include?(msg_type)
+        concat(content_tag :div, text, :class => "alert #{flash_class(msg_type)}")
+      end
+    end
+    nil
+  end
 end
